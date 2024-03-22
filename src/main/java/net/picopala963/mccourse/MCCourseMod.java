7f2fff986502e4746f1eb1a5963e5b8dddbb51fd
david <davidvallsdacosta1@gmail.com>
2024-03-22 08:22:10 +0100

    Initial forge setup for 1.20.X

--- src/main/java/net/picopala963/mccourse/MCCourseMod.java
+++ src/main/java/net/picopala963/mccourse/MCCourseMod.java
@@ -12,6 +12,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.picopala963.mccourse.block.ModBlocks;
+import net.picopala963.mccourse.item.ModCreativeModeTab;
 import net.picopala963.mccourse.item.ModItems;
 import org.slf4j.Logger;
 
@@ -26,6 +28,10 @@ public class MCCourseMod
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+
+        ModBlocks.register(modEventBus);
+
+        ModCreativeModeTab.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -42,6 +48,15 @@ public class MCCourseMod
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
             event.accept(ModItems.ALEXANDRITE);
             event.accept(ModItems.RAW_ALEXANDRITE);
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS){
+            event.accept(ModBlocks.ALEXANDRITE_BLOCK);
+            event.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK);
+            event.accept(ModBlocks.ALEXANDRITE_ORE);
+            event.accept(ModBlocks.DEEPSLATE_ALEXANDRITE_ORE);
+            event.accept(ModBlocks.END_STONE_ALEXANDRITE_ORE);
+            event.accept(ModBlocks.NETHER_ALEXANDRITE_ORE);
         }
     }
 
